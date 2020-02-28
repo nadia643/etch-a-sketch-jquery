75d6dd4a23ba1c0f6243e3d73ac7f74c80022d15
John Doe <nadia643@gmail.com>
2020-02-28 15:18:57 +0000

    added grid size choice buttons

---
+++ script.js
@@ -0,0 +1,54 @@
+
+    //jquery grid drawing    
+    const rows = 8;
+    const columns = 8;
+    const $row = $("<div />", {
+        class: 'row'
+    });
+    const $square = $("<div />", {
+        class: 'square'
+    });
+
+    $(document).ready(function () {
+        //add columns to the the temp row object
+        for (var i = 0; i < columns; i++) {
+            $row.append($square.clone());
+        }
+        //clone the temp row object with the columns to the wrapper
+        for (var i = 0; i < rows; i++) {
+            $("#wrapper").append($row.clone());
+        }
+    }); 
+        
+
+
+        //makes squares go pink
+        $(function() {
+            $('.square').hover(function() {
+                $(this).css('background-color', 'RGB(238, 120, 120');
+            }), 
+            function() {
+
+            }
+        })
+        //disco function
+        $(function() {
+            $('#disco').click(function() {
+                $(".square").each(function(index) {
+                    let colorR = Math.floor((Math.random() * 256));
+                    let colorG = Math.floor((Math.random() * 256));
+                    let colorB = Math.floor((Math.random() * 256));
+        $(this).css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+        });
+    });
+    });
+
+    //makes dropdown show upon click
+    function chooseGridSize() {
+        const click = document.getElementById("drop-content");
+        if (click.style.display === "none") {
+            click.style.display = "block";
+        } else {
+            click.style.display = "none";
+        }
+    }
