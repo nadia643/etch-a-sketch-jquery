4d870ef6b922d6ec7de764b089c640aeb968fe91
John Doe <nadia643@gmail.com>
2020-05-18 14:07:59 +0100

    reset colour of boxes to transparent

---
+++ README.md
@@ -0,0 +1,15 @@
+This project uses Javascript, jquery, and SASS.
+
+Installation
+
+$ git clone https://github.com/nadia643/etch-a-sketch-jquery
+
+Run Sever
+
+$ npm install
+
+Run App
+
+$ live-server
+
+Open http://127.0.0.1:8080/ to view it in the browser.
--- script.js
+++ script.js
@@ -1,173 +1,74 @@
-    //work on grid size buttons
-    // function Choice () {
-    //     let eight = document.getElementById("number-button-eight");
-    //     let ten = document.getElementById("ten");
-    //     let twelve = document.getElementById("twelve");
-    
-    //     if (eight.clicked == true) {
-    //         console.log("8");
-    //     } else if (ten.clicked == true) {
-    //         console.log("10");
-    //     } else {
-    //         console.log("12");
-    //     };
-    // };
-    
-    // Choice ();
-
-    // $( "button" ).click(function firstClick () {
+    // //jquery grid drawing    
+    // const rows = 8;
+    // const columns = 8;
+    // const $row = $("<div />", {
+    //     class: 'row'
+    // });
+    // const $square = $("<div />", {
+    //     class: 'square'
     // });
 
-    // const firstClick = function() {
-    //     const rows = 2;
-    //     const columns = 2;
-    //     const $row = $("<div />", {
-    //         class: 'row'
-    //     });
-    //     const $square = $("<div />", {
-    //         class: 'square'
-    //     });
-    
-    //     $(document).ready(function () {
-    //         //add columns to the the temp row object
-    //         for (var i = 0; i < columns; i++) {
-    //             $row.append($square.clone());
-    //         }
-    //         //clone the temp row object with the columns to the wrapper
-    //         for (var i = 0; i < rows; i++) {
-    //             $("#wrapper").append($row.clone());
-    //         }
-    //     });
+    // $(document).ready(function () {
+    //     //add columns to the the temp row object
+    //     for (var i = 0; i < columns; i++) {
+    //         $row.append($square.clone());
+    //     }
+    //     //clone the temp row object with the columns to the wrapper
+    //     for (var i = 0; i < rows; i++) {
+    //         $("#wrapper").append($row.clone());
+    //     }
+    // }); 
         
-    // }
+    //     //makes squares go pink
+    //     $(function() {
+    //         $('.square').hover(function() {
+    //             $(this).css('background-color', 'RGB(238, 120, 120');
+    //         }), 
+    //         function() {
 
-    //switch statement for rows and columns
-    // let allButtons = document.getElementsByTagName('button');
-
-    // for (let i = 0; i < allButtons.length; i++) {
-    //     allButtons[i].onclick = werewolfSituation;
-    // }
-    
-    // function werewolfSituation(e) {
-
-    // const moonPhase = this.innerHTML;
-    // switch (moonPhase) {
-    //     case "8x8": 
-    //     let rows = 8;
-    //     let columns = 8;
-    //     const $row = $("<div />", {
-    //         class: 'row'
-    //     });
-    //     const $square = $("<div />", {
-    //         class: 'square'
-    //     });
-    
-    //     $(document).ready(function () {
-    //         //add columns to the the temp row object
-    //         for (var i = 0; i < columns; i++) {
-    //             $row.append($square.clone());
     //         }
-    //         //clone the temp row object with the columns to the wrapper
-    //         for (var i = 0; i < rows; i++) {
-    //             $("#wrapper").append($row.clone());
-    //         }
-    //     }); 
-    //     break;
-    //     case "10x10": 
-    //     let rows = 10;
-    //     let columns = 10;
-    //     const $row = $("<div />", {
-    //         class: 'row'
-    //     });
-    //     const $square = $("<div />", {
-    //         class: 'square'
-    //     });
-    
-    //     $(document).ready(function () {
-    //         //add columns to the the temp row object
-    //         for (var i = 0; i < columns; i++) {
-    //             $row.append($square.clone());
-    //         }
-    //         //clone the temp row object with the columns to the wrapper
-    //         for (var i = 0; i < rows; i++) {
-    //             $("#wrapper").append($row.clone());
-    //         }
-    //     }); 
-    //     break;        
-    //     case "12x12": 
-    //     let rows = 12;
-    //     let columns = 12;
-    //     const $row = $("<div />", {
-    //         class: 'row'
-    //     });
-    //     const $square = $("<div />", {
-    //         class: 'square'
-    //     });
-    
-    //     $(document).ready(function () {
-    //         //add columns to the the temp row object
-    //         for (var i = 0; i < columns; i++) {
-    //             $row.append($square.clone());
-    //         }
-    //         //clone the temp row object with the columns to the wrapper
-    //         for (var i = 0; i < rows; i++) {
-    //             $("#wrapper").append($row.clone());
-    //         }
-    //     }); 
-    //     break;
-
-    // }
-    // }
-    //jquery grid drawing    
-    const rows = 8;
-    const columns = 8;
-    const $row = $("<div />", {
-        class: 'row'
-    });
-    const $square = $("<div />", {
-        class: 'square'
-    });
-
-    $(document).ready(function () {
-        //add columns to the the temp row object
-        for (var i = 0; i < columns; i++) {
-            $row.append($square.clone());
-        }
-        //clone the temp row object with the columns to the wrapper
-        for (var i = 0; i < rows; i++) {
-            $("#wrapper").append($row.clone());
-        }
-    }); 
-        
-
-
-        //makes squares go pink
-        $(function() {
-            $('.square').hover(function() {
-                $(this).css('background-color', 'RGB(238, 120, 120');
-            }), 
-            function() {
-
-            }
-        })
+    //     })
         //disco function
-        $(function() {
-            $('#disco').click(function() {
-                $(".square").each(function(index) {
-                    let colorR = Math.floor((Math.random() * 256));
-                    let colorG = Math.floor((Math.random() * 256));
-                    let colorB = Math.floor((Math.random() * 256));
-        $(this).css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
-        });
-    });
-    });
+    //     $(function() {
+    //         $('#disco').click(function() {
+    //             $(".div").each(function(index) {
+    //                 let colorR = Math.floor((Math.random() * 256));
+    //                 let colorG = Math.floor((Math.random() * 256));
+    //                 let colorB = Math.floor((Math.random() * 256));
+    //     $(this).css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+    //     });
+    // });
+    // });
 
-    //makes dropdown show upon click
-    function chooseGridSize() {
-        const click = document.getElementById("drop-content");
-        if (click.style.display === "none") {
-            click.style.display = "block";
-        } else {
-            click.style.display = "none";
+
+function createGrid() {
+    document.getElementById('container').innerHTML = "";
+    let numberOfRows = prompt("Enter a number");
+    if (isNaN(numberOfRows)) {
+        alert("That's not a number. Please try again!")
+    } else {
+    console.log(numberOfRows);
+    for (let i=0; i < numberOfRows; i++) {
+        let divRow = document.createElement("div");
+        document.getElementById('container').appendChild(divRow);
+    for(let y=0; y < numberOfRows; y++) {
+        let divCol = document.createElement('div');
+        
+            divCol.addEventListener("mouseenter", function() {
+                function generateRandomColor()
+            {
+            let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+            return randomColor;
         }
+        this.style.backgroundColor = generateRandomColor();
+            });
+            divRow.appendChild(divCol);
+    }}
     }
+    
+}
+
+function resetColorOfBoxes() {
+    boxes = document.querySelectorAll('div');
+    boxes.forEach(box => box.style.backgroundColor = "transparent");
+  }
